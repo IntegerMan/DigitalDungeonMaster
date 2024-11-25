b0d50928c7f5e760bb792aa97a967ae9b53d3049
Matt Eland <Matt.Eland@GMail.com>
2024-11-24 19:35:08 -0500

    Initial empty code commit

---
+++ MattEland.BasementsAndBasilisks.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MattEland.BasementsAndBasilisks.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
