f7ab86597feee20d68850fd36d33d43f12d7871e
Matt Eland <Matt.Eland@GMail.com>
2024-11-28 12:23:14 -0500

    Adding in Storage

--- MattEland.BasementsAndBasilisks.ConsoleApp/BasiliskConfig.cs
+++ MattEland.BasementsAndBasilisks.ConsoleApp/BasiliskConfig.cs
@@ -5,4 +5,6 @@ public class BasiliskConfig
     public string AzureOpenAiKey { get; set; }
     public string AzureOpenAiEndpoint { get; set; }
     public string AzureOpenAiDeploymentName { get; set; }
+    public string AzureStorageKey { get; set; }
+    public string AzureStorageEndpoint { get; set; }
 }
