719cf54f44567b4ad94ceb48506e5113802d19e5
Matt Eland <Matt.Eland@GMail.com>
2024-12-07 16:16:37 -0500

    Now invoking the adventure service on start

--- MattEland.DigitalDungeonMaster/Services/AdventuresService.cs
+++ MattEland.DigitalDungeonMaster/Services/AdventuresService.cs
@@ -6,16 +6,18 @@ namespace MattEland.DigitalDungeonMaster.Services;
 public class AdventuresService
 {
     private readonly StorageDataService _storageService;
+    private readonly RequestContextService _context;
 
-    public AdventuresService(StorageDataService storageService)
+    public AdventuresService(StorageDataService storageService, RequestContextService context)
     {
         _storageService = storageService;
+        _context = context;
     }
     
     public async Task CreateAdventureAsync(AdventureInfo adventure)
     {
         // Add an entry to table storage
-        await _storageService.CreateTableEntryAsync("adventures", new TableEntity()
+        await _storageService.CreateTableEntryAsync("adventures", new TableEntity
         {
             PartitionKey = adventure.Owner,
             RowKey = adventure.RowKey,
@@ -24,5 +26,8 @@ public class AdventuresService
             ["Container"] = adventure.Container,
             ["Ruleset"] = adventure.Ruleset
         });
+        
+        // Set our current adventure to this adventure
+        _context.CurrentAdventure = adventure;
     }
 }
