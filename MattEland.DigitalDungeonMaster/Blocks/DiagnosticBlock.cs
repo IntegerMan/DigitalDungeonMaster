6a036038f3724019b6313fd3ebe28f45348f7e55
Matt Eland <Matt.Eland@GMail.com>
2024-12-09 00:26:16 -0500

    Removed no longer needed diagnostic block

--- MattEland.DigitalDungeonMaster/Blocks/DiagnosticBlock.cs
+++
@@ -1,7 +0,0 @@
-namespace MattEland.DigitalDungeonMaster.Blocks;
-
-public class DiagnosticBlock : ChatBlockBase
-{
-    public string? Metadata { get; init; }
-    public required string Header { get; set; }
-}
